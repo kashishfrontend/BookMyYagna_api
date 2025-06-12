02be9e86e53c60d4dd556adb5055fd63adba7a12
NakulRana67 <apideveloper@earninghandle.com>
2025-06-12 10:11:46 +0530

    update bg in list of pooja section

--- src/Page/List-Of-Pooja/ListOfPooja.jsx
+++ src/Page/List-Of-Pooja/ListOfPooja.jsx
@@ -45,10 +45,6 @@ const PoojaSlider = () => {
         console.error('Error fetching pandits:', error);
       });
   }, []);
-
-
-
-
   return (
     <>
       <section className="pooja-slider-section mb-5">
